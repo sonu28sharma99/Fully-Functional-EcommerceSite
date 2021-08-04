48c627f69f37fc5f4340531b085bb0205305bfd2
sonu28shamra99 <sonu28shamra99@gmail.com>
2021-08-05 03:44:28 +0530

    successfully created our server

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,16 @@
 import express from 'express';
+import data from './data.js';
 
 const app = express();
 
-app.get('/', (req, res) =>{
-    res.send('Server is ready');
+app.get('/api/products', (req, res) => {
+  res.send(data.products);
 });
 
-app.listen(5000, ()=>{
-    console.log('Serve at http://localhost:5000');
+app.get('/', (req, res) => {
+  res.send('Server is ready');
+});
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Serve at http://localhost:${port}`);
 });
