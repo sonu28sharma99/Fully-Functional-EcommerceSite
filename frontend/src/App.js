5d1df43e61396a7e92c552d7f4ff997f9a0a01ee
sonu28shamra99 <sonu28shamra99@gmail.com>
2021-08-05 02:15:34 +0530

    product screen created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
-import Product from './components/Product';
-import data from './data';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+
 function App() {
   return (
     <BrowserRouter>
@@ -20,13 +21,6 @@ function App() {
         <main>
           <Route path="/product/:id" component={ProductScreen}></Route>
           <Route path="/" component={HomeScreen} exact></Route>
-          <div>
-            <div className="row center">
-              {data.products.map((product) => (
-                <Product key={product._id} product={product}></Product>
-              ))}
-            </div>
-          </div>
         </main>
         <footer className="row center">All right reserved</footer>
       </div>
