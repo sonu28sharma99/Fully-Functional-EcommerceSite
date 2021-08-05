960494f8678dca81ca46946f609e89a43213f977
sonu28shamra99 <sonu28shamra99@gmail.com>
2021-08-05 06:55:36 +0530

    adding Loading box

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,15 +1,34 @@
-import React from 'react'
-import data from '../data';
-import Product from '../components/Product';  
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import Product from '../components/Product';
 
 export default function HomeScreen() {
-    return (
-        <div>
-        <div className="row center">
-          {data.products.map((product) => (
-            <Product key={product._id} product={product}></Product>
-          ))}
-        </div>
-      </div>
-    )
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  useEffect(() => {
+    const fecthData = async () => {
+      try {
+        setLoading(true);
+        const { data } = await axios.get('/api/products')
+        setProducts(data);
+      } catch (err) {
+        setError(err.message);
+        setLoading(false);
+      }
+    };
+    fecthData();
+  }, [])
+  return (
+    <div>
+      {loading ? (<LoadingBox></LoadingBox>) :
+        error ? (<MessageBox>{error}</MessageBox>) :
+          (<div className="row center">
+            {products.map((product) => (
+              <Product key={product._id} product={product}></Product>
+            ))}
+          </div>
+          )}
+    </div>
+  );
 }
